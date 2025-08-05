2101044d8a0b3004ca5715339343513863029248
parthiban <parthiban@longshore.io>
2025-08-05 12:26:02 +0530

    first commit

--- src/utils/peer.js
+++ src/utils/peer.js
@@ -1,15 +1,19 @@
-import { Peer } from 'peerjs';
+import { Peer } from "peerjs";
 export function createPeer(id) {
   return new Peer(id, {
     host: "0.peerjs.com",
     port: 443,
-    path: '/',
+    path: "/",
     secure: true,
     config: {
-    iceServers: [
-      { urls: "stun:stun.l.google.com:19302" }, // Google's STUN
-      { url: "relay1.expressturn.com:3480", username: "000000002069734054", credential: 'cBRqCyW/nnXXTUu8vpRXGC16XT4=' }
-    ]
-  }
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" }, // Google's STUN
+        {
+          url: "turn:turn.anyfirewall.com:443?transport=tcp",
+          credential: "webrtc",
+          username: "webrtc",
+        },
+      ],
+    },
   });
 }
