7139a4e804151b61c6d1c99e59033b39eee886c4
parthiban <parthiban@longshore.io>
2025-08-05 13:33:44 +0530

    first commit

--- src/utils/peer.js
+++ src/utils/peer.js
@@ -7,14 +7,30 @@ export function createPeer(id) {
     secure: true,
     config: {
       iceServers: [
-        { urls: "stun:stun.l.google.com:19302" }, // Google's STUN
+        { urls: ["stun:bn-turn1.xirsys.com"] },
         {
-          url: "turn:turn.anyfirewall.com:443?transport=tcp",
-          credential: "webrtc",
-          username: "webrtc",
+          username:
+            "CVx15pqsYzn-2znQE9nRIiWFmovI-uxDi4gFeuQIWMmrg3FT6ao9FiAsOZYsd-B4AAAAAGiRuipwYXJ0aGliYW4=",
+          credential: "48a5c96c-71d2-11f0-8421-0242ac140004",
+          urls: [
+            "turn:bn-turn1.xirsys.com:80?transport=udp",
+            "turn:bn-turn1.xirsys.com:3478?transport=udp",
+            "turn:bn-turn1.xirsys.com:80?transport=tcp",
+            "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+            "turns:bn-turn1.xirsys.com:443?transport=tcp",
+            "turns:bn-turn1.xirsys.com:5349?transport=tcp",
+          ],
         },
       ],
-    iceTransportPolicy: "all", // allows relay fallback
+      //   iceServers: [
+      //     { urls: "stun:stun.l.google.com:19302" }, // Google's STUN
+      //     {
+      //       url: "turn:turn.anyfirewall.com:443?transport=tcp",
+      //       credential: "webrtc",
+      //       username: "webrtc",
+      //     },
+      //   ],
+      iceTransportPolicy: "all", // allows relay fallback
     },
   });
 }
